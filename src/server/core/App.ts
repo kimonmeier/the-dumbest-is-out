8c011bf763bcbb34796b352cfa6b56f8da823c7c
Kimon Meier <kimon.meier@gmail.com>
2025-10-07 03:57:27 +0200

    Added initialitaion of eventbus and added the playermanger

--- src/server/core/App.ts
+++ src/server/core/App.ts
@@ -5,6 +5,10 @@ import type { ServerToClientEvents } from '@gameshow-lib/message/ServerToClientE
 import { randomUUID } from 'node:crypto';
 import type { PlayerId } from '@gameshow-lib/message/OpaqueTypes';
 import { PUBLIC_ROOM_CODE } from '@server/Konst';
+import { PlayerManager } from './PlayerManager';
+import { createInMemoryEventBus } from '@danielemariani/ts-event-bus';
+import type { EventDeclaration } from '@server/events/EventDeclaration';
+import type { GameshowEventBus } from '@server/events/Eventbus';
 
 export type AppServer = Server<ClientToServerEvents, ServerToClientEvents, object, object>;
 export type AppSocket = Socket<ClientToServerEvents, ServerToClientEvents, object, object>;
@@ -12,13 +16,18 @@ export type AppSocket = Socket<ClientToServerEvents, ServerToClientEvents, objec
 export class App {
 	private readonly webSocket: AppServer;
 	private readonly historyManager: HistoryManager;
+	private readonly playerManager: PlayerManager;
+	private readonly eventBus: GameshowEventBus;
 
 	public constructor() {
 		this.webSocket = new Server<ClientToServerEvents, ServerToClientEvents, object, object>({
 			connectionStateRecovery: {}
 		});
 
+		this.eventBus = createInMemoryEventBus<EventDeclaration>();
+
 		this.historyManager = new HistoryManager(this.webSocket);
+		this.playerManager = new PlayerManager(this.historyManager, this.webSocket);
 	}
 
 	public startApp(): void {
@@ -35,6 +44,7 @@ export class App {
 			socket.join(PUBLIC_ROOM_CODE);
 
 			this.historyManager.registerSocket(socket, userId);
+			this.playerManager.registerSocket(socket, userId);
 		});
 
 		this.webSocket.listen(Deno.env.get('PORT') ?? 3000);
