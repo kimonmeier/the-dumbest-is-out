5d132aee346ffe4ed7c567200528b6f39cd7e494
Kimon Meier <kimon.meier@gmail.com>
2025-10-07 03:54:58 +0200

    Created Eventbus and installed correct package

---
+++ src/server/events/Eventbus.ts
@@ -0,0 +1,4 @@
+import type { EventBus } from '@danielemariani/ts-event-bus';
+import type { EventDeclaration } from './EventDeclaration';
+
+export type GameshowEventBus = EventBus<EventDeclaration>;
