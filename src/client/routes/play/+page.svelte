bd8f7983863f4b8c810afc64221bdc65330f7ff3
Kimon Meier <kimon.meier@gmail.com>
2025-10-13 07:35:38 +0200

    Removed usless mehtods

--- src/client/routes/play/+page.svelte
+++ src/client/routes/play/+page.svelte
@@ -2,22 +2,6 @@
 	import GameMaster from '@client/lib/components/game/GameMaster.svelte';
 	import PlayerCard from '@client/lib/components/game/PlayerCard.svelte';
 	import { playerStore } from '@client/lib/stores/PlayerStore';
-
-	function getCols(playerLength: number): number {
-		if (playerLength > 5) {
-			return 6;
-		}
-
-		return 5;
-	}
-
-	function getRows(playerLength: number): number {
-		if (playerLength > 5) {
-			return 4;
-		}
-
-		return 3;
-	}
 </script>
 
 <div class="w-full h-full grid grid-cols-3 grid-rows-3">
