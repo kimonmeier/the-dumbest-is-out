686089447210ccabe88b5637733e5e1fd4e6f931
Kimon Meier <kimon.meier@gmail.com>
2025-10-19 20:58:23 +0200

    Fixed iframe warning

--- src/client/lib/components/cam/VodNinjaWrapper.svelte
+++ src/client/lib/components/cam/VodNinjaWrapper.svelte
@@ -12,7 +12,7 @@
 		class="rounded-t-2xl aspect-video {className}"
 		src="{url}&cleanoutput&transparent&as&videobitrate=6000&deafen&noaudioprocessing"
 		title="Kamera"
-	/>
+	></iframe>
 {:else}
 	<div class="rounded-t-3xl aspect-video flex items-center justify-center h-full w-full">
 		<Icon icon="mdi:camera-off" height="80px" />
